88fdff268f08d941e37da3b855cb296aaa2061a7
pgoel92 <prateek.goel06@gmail.com>
2018-07-22 11:23:48 -0700

    Stop using .view to transpose while creating input and target tensors

--- rnn_word_lm/main.py
+++ rnn_word_lm/main.py
@@ -13,6 +13,7 @@ from evaluate import evaluate
 from generate import generate
 import matplotlib
 import matplotlib.pyplot as plt
+import sys
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--lr', type=float, default=0.01, help='learning rate')
@@ -89,12 +90,16 @@ def lineToTensor(line):
     return input_tensor, target_tensor
 
 def lineToTensor_continuous(line):
-    words = line.split()
-    word_tensors = map(encode_word, words)
+    word_tensors = map(encode_word, line)
     input_tensor = listToTensor(word_tensors[:len(word_tensors) - 1])
-    target_tensor = torch.LongTensor([vocab[word] for word in words[1:]])
+    target_tensor = torch.LongTensor([vocab[word] for word in line[1:]])
 
     return input_tensor, target_tensor
+
+def lineToIndices(line):
+    input = torch.FloatTensor([vocab[word] for word in line[:len(line) - 1]])
+    targets = torch.LongTensor([vocab[word] for word in line[1:]])
+    return input, targets
 
 def get_batch(lines, batch_number, num_tokens, vocab_size, batch_size):
     batch_lines = lines[batch_number * batch_size:(batch_number + 1) * batch_size]
@@ -109,21 +114,18 @@ def get_batch(lines, batch_number, num_tokens, vocab_size, batch_size):
     return Variable(input_batch.view(num_tokens + 1, batch_size, vocab_size)), Variable(target_batch.view(num_tokens + 1, batch_size))
 
 BPTT = 35
-def get_batch_continuous(lines, batch_number, num_tokens, vocab_size, batch_size):
-    lines = ['<start> ' + line + ' <end>' for line in lines]
-    text = ' '.join(lines)
-    nwords = len(text.split())
-    # batch_lines = lines[batch_number * batch_size:(batch_number + 1) * batch_size]
-    # The number of steps is num_tokens + 1 because of <start> and <end> being appended to each input and target sequence respectively
-    input_batch = torch.zeros(batch_size, BPTT - 1, vocab_size)
-    target_batch = torch.zeros(batch_size, BPTT - 1).type(torch.LongTensor)
+def get_batch_continuous(text, batch_number, device):
+    nwords = len(text)
+    input_batch = torch.zeros(BPTT, args.bsz).type(torch.LongTensor).to(device)
+    target_batch = torch.zeros(BPTT, args.bsz).type(torch.LongTensor).to(device)
     for i in range(batch_size):
-        line = ' '.join(text.split()[i*BPTT:min(i*BPTT + BPTT, nwords)])
-        input, target = lineToTensor_continuous(line)
-        input_batch[i] = input.squeeze()
-        target_batch[i] = target
+        line = text[i*BPTT:min(i*BPTT + BPTT + 1, nwords)]
+        input, target = lineToIndices(line)
+	for j in range(BPTT):
+            input_batch[j][i] = input[j]
+            target_batch[j][i] = target[j]
 
-    return Variable(input_batch.view(BPTT - 1, batch_size, vocab_size)), Variable(target_batch.view(BPTT - 1, batch_size))
+    return input_batch, target_batch
 
 def randomChoice(l):
     return l[random.randint(0, len(l) - 1)]
@@ -168,15 +170,28 @@ def train(rnn, hidden, criterion, learning_rate, input_batch, targets):
     rnn.zero_grad()
 
     hidden = repackage_hidden(hidden)
-    outputs, hidden = rnn.forward(input_batch, hidden)
-    loss = criterion(outputs.view(-1, outputs.size()[2]), targets)
+    output, hidden = rnn.forward(input_batch, hidden)
+    loss = criterion(output.view(-1, output.size()[2]), targets.view(-1))
     loss.backward()
     optimizer.step()
 
-    #for p in rnn.parameters():
-    #    p.data.add_(-learning_rate, p.grad.data)
+def get_batch_pytorch(source, i):
+    seq_len = min(BPTT, len(source) - 1 - i)
+    data = source[i:i+seq_len]
+    target = source[i+1:i+1+seq_len].view(-1)
+    return data, target
 
-    return outputs, loss.item(), hidden
+def replace_words_with_indices(text):
+    return torch.LongTensor([vocab[word] for word in text])
+
+def batchify(data, bsz):
+    # Work out how cleanly we can divide the dataset into bsz parts.
+    nbatch = data.size(0) // bsz 
+    # Trim off any extra elements that wouldn't cleanly fit (remainders).
+    data = data.narrow(0, 0, nbatch * bsz)
+    # Evenly divide the data across the bsz batches.
+    data = data.view(bsz, -1).t().contiguous()
+    return data.to(device)
 
 def main(device):
     lines, vocab_size = readData(args.data + '/train.txt', args.vsize)
@@ -184,52 +199,37 @@ def main(device):
     with open('vocab.pickle', 'wb') as handle:
         pickle.dump(vocab, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
-    rnn = model.RNNModel('LSTM', vocab_size, args.nhid, args.nhid, 2, 0.2)
-    if args.cuda:
-        rnn.cuda()
+    rnn = model.RNNModel('LSTM', vocab_size, args.nhid, args.nhid, 2, 0.2).to(device)
+    #if args.cuda:
+    #    rnn.cuda()
     criterion = nn.CrossEntropyLoss()
-    learning_rate = args.lr
 
-    running_loss = 0
-    num_epochs = 300
-    num_iterations = len(lines) / args.bsz
-    print_every = num_iterations/10 - 1
+    num_epochs = 40
     start_time = timeit.default_timer()
-    #hidden = rnn.initHidden(args.bsz)
     num_tokens = len(lines[0].split())
-    training_loss = []
-    dev_loss = []
-    dev_perplexity = []
-    test_loss = []
-    prev_dev_perplexity = 9999999999;
+    lines = ['<start> ' + line + ' <end>' for line in lines]
+    text = ' '.join(lines).split()
+    num_batches = len(text) / (BPTT * args.bsz)
+    print("# of batches %d" % num_batches)
     for e in range(num_epochs):
-        for i in range(num_iterations):
+        for i in range(num_batches):
             hidden = rnn.initHidden(args.bsz, device)
-            input_batch, target_batch = get_batch_continuous(lines, i, num_tokens, vocab_size, args.bsz)
-            if args.cuda:
-                input_batch = input_batch.cuda()
-                target_batch = target_batch.cuda()
-                hidden = (hidden[0].cuda(), hidden[1].cuda())
-            outputs, loss, hidden = train(rnn, hidden, criterion, learning_rate, input_batch, target_batch)
-
-            # print(str(loss) + " # " + lines[i%len(lines)])
-            # running_loss += loss
+            input_batch, target_batch = get_batch_continuous(text, i, device)
+            #if args.cuda:
+            #    input_batch = input_batch.cuda()
+            #    target_batch = target_batch.cuda()
+            #    hidden = (hidden[0].cuda(), hidden[1].cuda())
+            train(rnn, hidden, criterion, learning_rate, input_batch, target_batch)
 
         elapsed = timeit.default_timer() - start_time
         print('##################')
         print('Epoch %d :' % e)
-        #print('Time elapsed : %s, Projected epoch training time : %s' % (get_readable_time(int(elapsed)), get_readable_time(int((elapsed/(i + e*num_iterations))*num_iterations))))
         print('Time elapsed : %s' % (get_readable_time(int(elapsed))))
-        #print('Training loss : %.4f' % (running_loss/num_iterations))
-        #training_loss.append(running_loss/num_iterations)
-        #running_loss = 0
 
         if args.cuda:
             loss, perp = evaluate(args.data + '/validation.txt', rnn, vocab, cuda=True)
         else:
             loss, perp = evaluate(args.data + '/validation.txt', rnn, vocab)
-        dev_loss.append(loss)
-        dev_perplexity.append(perp)
         print('Validation loss : %.1f' % loss)
         print('Validation perplexity : %.1f' % perp)
         rnn.eval()
@@ -239,10 +239,7 @@ def main(device):
             print(sample)
         with open('model.pt', 'wb') as f:
             torch.save(rnn, f)
-        if perp > prev_dev_perplexity:
-            learning_rate /= 4
-        prev_dev_perplexity = perp
-    plotTrainingVsDevLoss(training_loss, dev_loss, 'training_vs_dev_loss.png')
+    #plotTrainingVsDevLoss(training_loss, dev_loss, 'training_vs_dev_loss.png')
 
 if __name__ == "__main__":
     print "V3.3"
